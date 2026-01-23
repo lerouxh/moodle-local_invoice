b9b08bc1b6befb0839e7af9ae4f421d29c145c59
lerouxh <lerouxh@gmail.com>
2026-01-23 15:36:26 +0200

    Bump version for resubmission

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_invoice';  // Full name of the plugin (used for diagnostics).
-$plugin->version = 2026012202;  // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2026012300;  // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2023100900.00;    // Moodle 4.3
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0.4';
